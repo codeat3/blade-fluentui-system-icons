ffee642f330ce79066505efd197b6a1951b0e546
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-03-28 12:46:29 +0000

    Fix styling

--- src/BladeFluentUiSystemIconsServiceProvider.php
+++ src/BladeFluentUiSystemIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeFluentUiSystemIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-fluentui-system-icons', []);
 
-            $factory->add('fluentui-system-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('fluentui-system-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-fluentui-system-icons.php', 'blade-fluentui-system-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-fluentui-system-icons.php', 'blade-fluentui-system-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-fluentui-system-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-fluentui-system-icons'),
             ], 'blade-fluentui-system-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-fluentui-system-icons.php' => $this->app->configPath('blade-fluentui-system-icons.php'),
+                __DIR__ . '/../config/blade-fluentui-system-icons.php' => $this->app->configPath('blade-fluentui-system-icons.php'),
             ], 'blade-fluentui-system-icons-config');
         }
     }
